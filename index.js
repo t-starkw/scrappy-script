b844114b086de64aee9af7894fb6e55eeadc705d
Tessa Starkweather <108361868+t-starkw@users.noreply.github.com>
2023-04-25 15:31:44 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const PORT = 3001
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+const fs = require('fs');
+
+const url = 'https://quotes.toscrape.com/'
+
+const app = express();
+
+axios.get(url)
+    .then((response) => {
+        let $ = cheerio.load(response.data);
+        let quotes = [];
+
+        $('div.col-md-8 div').each((index, element) => {
+            quotes.push({
+                title: $(element).find('span.text').text().trim()
+            })
+        });
+
+        fs.writeFile('./quotes.json', JSON.stringify(quotes), (error) => {
+            if (error) throw error;
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}...`));
